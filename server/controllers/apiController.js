fa6b801f1ee42ea10ad98113b2614c03f4686a7f
Keshal Desai <novalearngood@gmail.com>
2017-06-13 16:38:29 -0400

    reduce implemented

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -7,7 +7,9 @@ module.exports = function (app) {
 	//get stock data
 	app.get('/api/stock/:symbol', function (req, res) {
 		const { symbol } = req.params;
-		const uri = `https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20100101&ticker=${symbol}&qopts.columns=date,open&api_key=${key}`;
+		const date = new Date();
+		const year = date.getUTCFullYear() - 1;
+		const uri = `https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=${year}0601&ticker=${symbol}&qopts.columns=date,open&api_key=${key}`;
 		request.get(uri, (err, response, body) => {
 			if (err) {
 				return errorHandler(err, res, response.statusCode);
@@ -16,7 +18,34 @@ module.exports = function (app) {
 			if (data.length === 0) {
 				return errorHandler(err, res, 404);
 			}
-			return res.json({ data });
+			const callback = (prev) => {
+				res.json(prev);
+			}
+			data.reduce((prev, curr, ind) => {
+				const dateStamp = curr[0].split('-');
+				const year = dateStamp[0];
+				const month = dateStamp[1];
+				const price = curr[1];
+				if (!prev[year]) {
+					prev[year] = {};
+				}
+				if (!prev[year][month]) {
+					prev[year][month] = {
+						price,
+						count: 1
+					}
+				} else {
+					const newPrice = parseFloat(price + prev[year][month].price).toFixed(2);
+					prev[year][month] = {
+						price: newPrice,
+						count: prev[year][month].count + 1
+					}
+				}
+				if (ind === data.length - 1) {
+					callback(prev);
+				}
+				return prev;
+			}, {})
 		});
 	});
 }
